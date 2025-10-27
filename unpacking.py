84a41ccd50fe0bd2b313c8ce67a5923aa22dc2cb
Charles Juma <cjuma0909@gmail.com>
2025-10-27 17:17:57 +0300

    second commit

---
+++ unpacking.py
@@ -0,0 +1,7 @@
+#unpacking is a technique in python that allows you to extract values into variables from a collection of values in a list or tuple
+#eg, unpacking a list of fruits
+fruits = ["apple","orange","banana"]
+A, B, C = fruits
+print(A)
+print(B)
+print(C)
--- variable.py
+++ variable.py
@@ -14,3 +14,15 @@ This is another multiline comment in python
 it is a literal string and it is not assigned to any variable
 python will therefore ignore it
 """
+#python allows you to assign values to multiple variables in one line
+#Note: Make sure the number of variables matches the number of values, or else you will get an error
+z,r,q = "orange", "banana", "melon"
+print(z)
+print(r)
+print(q)
+
+#You can also assign a single value to multiple variables in one line as below
+a = b = c = "fish"
+print(a)
+print(b)
+print(c)
