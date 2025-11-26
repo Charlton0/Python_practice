2849ebdbb37ad4fd0520617b8a78654560994827
Charles Juma <cjuma0909@gmail.com>
2025-11-26 22:38:50 +0300

    new commit

--- FormatStrings.py
+++ FormatStrings.py
@@ -21,10 +21,10 @@ myorder  = "I want {} pieces of item {} for {} dollars"
 print(myorder.format(quantity, itemno, price))
 
 # You can use the index numbers to be ensure that the arguments are placed in the correct placeholders
-quantity = 3
+"""quantity = 3
 itemno = 567
 price = 49.95
 myorder = "I want to pay {2} dollars for {0} pieces of item {1}."
 print(myorder.format(quantity, itemno, price))
-
+"""
  
